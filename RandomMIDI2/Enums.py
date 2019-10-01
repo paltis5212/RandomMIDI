a22a3995e3018b3829f67a57aa9148cec0a2cf7c
paltis5212 <paltis5212@gmail.com>
2019-10-01 18:21:05 +0800

    Add files via upload

--- RandomMIDI2/Enums.py
+++ RandomMIDI2/Enums.py
@@ -11,3 +11,27 @@ class NoteNum(Enum):
     B = 86
     lifting = 12
     order = ["C", "D", "E", "F", "G", "A", "B"]
+
+
+class LimitData(Enum):
+    limit = "limit"
+    speed = "speed"
+    scale = "scale"
+    sentenceCount = "sentenceCount"
+    program = "program"
+    title = ["speed", "scale", "sentenceCount", "program"]
+
+
+class WeightsData(Enum):
+    weights = "weights"
+    scale = "scale"
+    tempo = "tempo"
+    scaleFluctuation = "scaleFluctuation"
+    wordCount = "wordCount"
+    endTempo = "endTempo"
+    endWordCount = "endWordCount"
+    repeatCount = "repeatCount"
+    repeatChange = "repeatChange"
+    strength = "strength"
+    title = ["scale", "tempo", "scaleFluctuation", "wordCount", "endTempo",
+             "endWordCount", "repeatCount", "repeatChange", "strength"]
--- RandomMIDI2/ObjectModel.py
+++ RandomMIDI2/ObjectModel.py
@@ -1,4 +1,7 @@
 import random
+from openpyxl import load_workbook
+from ComputeUtils import ComputeUtils
+from Enums import *
 
 
 class Weights():
@@ -33,8 +36,11 @@ class Sentence():
         isInit = False
         # 指定重複句子
         if repeatFrom is not None:
-            self.notes = repeatFrom.notes
+            self.startWordCount = repeatFrom.startWordCount
+            self.endWordCount = repeatFrom.endWordCount
+            self.wordCount = repeatFrom.wordCount
             self.repeatFrom = repeatFrom.pos
+            self.notes = repeatFrom.notes
         # 新語句生成字數規則
         elif wordWeights is not None and endWeights is not None:
             while self.wordCount < self.endWordCount or not isInit:
@@ -45,24 +51,51 @@ class Sentence():
         else:
             print("ERROR Sentence.__init__")
 
+    def randomChangeNotes(self, changeCount: int, music: dict, sentence: Sentence, index: int, scaleFluctuationWeight: Weights, scaleWeight: Weights, tempoWeight: Weights, strengthWeight: Weights):
+        """ 改變指定數量的音符 """
+        changeIndex = []
+        index = 0
+        # 不能超過句子音符數(外面要先過濾一次，通常不希望這條成立)
+        if changeCount > self.wordCount:
+            changeCount = self.wordCount
+        # 生成要改變的音符編號(不重複)
+        while index < changeCount:
+            num = random.randint(0, self.wordCount)
+            if num not in changeIndex:
+                changeIndex.append(num)
+                index += 1
+        # 生成新音符
+        for index in changeIndex:
+            self.notes[index] = ComputeUtils().createNewNoteFromPass(
+                music, sentence, index, scaleFluctuationWeight, scaleWeight, tempoWeight, strengthWeight)
 
-class LimitData():
-    speed: dict
-    scale: dict
-    sentenceCount: dict
-    program: dict
-    title = ["speed", "scale", "sentenceCount", "program"]
+    def AllTable(self):
+        def __init__(self, path: str):
+            wb = load_workbook(path)
+            ws = wb.active
+            titleRow = {}
+            nowTable = LimitData.limit
+            try:
+                # 獲得標題 row
+                for cell in ws["A"]:
+                    if cell.value == LimitData.limit or cell.value == WeightsData.weights:
+                        nowTable = cell.value
+                        titleRow[nowTable] = {}
+                    elif cell.value != None:
+                        titleRow[nowTable][cell.value] = cell.row
 
-
-class WeightsData():
-    scale: dict
-    tempo: dict
-    scaleFluctuation: dict
-    wordCount: dict
-    endTempo: dict
-    endWordCount: dict
-    repeatCount: dict
-    repeatChange: dict
-    strength: dict
-    title = ["scale", "tempo", "scaleFluctuation", "wordCount", "endTempo",
-             "endWordCount", "repeatCount", "repeatChange", "strength"]
+                allTable = {}
+                # 讀取 row 資料
+                for table in titleRow:
+                    allTable[table] = {}
+                    for title in titleRow[table]:
+                        # 讀取 row 在 sheet 資料，給 key
+                        allTable[table][title] = {}
+                        for x in ws[titleRow[table][title]]:
+                            key = ws.cell(row=x.row - 1, column=x.column).value
+                            if x.value != None and key != None:
+                                allTable[table][title][key] = x.value
+                self.limit = allTable[LimitData.limit]
+                self.weights = allTable[WeightsData.weights]
+            except:
+                print("ERROR AllTable.__init__")
